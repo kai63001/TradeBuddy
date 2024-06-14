34a61569d64d5574311f1671400b329762583a96
kai63001 <supanat0245@gmail.com>
2024-06-14 18:19:57 +0700

    fix: update selction date

--- trade_buddy_app/lib/components/add_trade/add_trade_manually.dart
+++ trade_buddy_app/lib/components/add_trade/add_trade_manually.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart';
+import 'package:intl/intl.dart';
 
 void showAddTradeManually(BuildContext context) {
+  DateTime now = DateTime.now();
+  DateFormat formattedDate = DateFormat('dd-MMMM-yyyy');
+  DateFormat formatTime = DateFormat('kk:mm');
+
   showModalBottomSheet(
     backgroundColor: const Color(0xff222222),
     context: context,
@@ -46,24 +52,76 @@ void showAddTradeManually(BuildContext context) {
                       borderRadius: BorderRadius.all(Radius.circular(8)),
                     )),
               ),
-              const SizedBox(height: 10),
-              //input date and time
-              const TextField(
-                decoration: InputDecoration(
-                    hintText: '06/10/2024 12:00 PM',
-                    labelText: 'Date and Time',
-                    labelStyle: TextStyle(color: Colors.white),
-                    border: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 207, 207, 207)),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
+              const SizedBox(height: 20),
+              //selection date and time with label
+              GestureDetector(
+                onTap: () => {
+                  DatePicker.showDateTimePicker(context, showTitleActions: true,
+                      onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime.now(), locale: LocaleType.en)
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 207, 207, 207)),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: Center(
+                            child: Text(
+                              formattedDate.format(now),
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 201, 201, 201)),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    )),
-              ),
+                    const SizedBox(width: 10),
+                    Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 207, 207, 207)),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        child: Center(
+                          child: Text(
+                            formatTime.format(now),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+
+              // TextButton(
+              //     onPressed: () {
+              //       DatePicker.showDateTimePicker(context, showTitleActions: true,
+              //           onChanged: (date) {
+              //         print('change $date');
+              //       }, onConfirm: (date) {
+              //         print('confirm $date');
+              //       }, currentTime: DateTime.now(), locale: LocaleType.en);
+              //     },
+              //     child: const Text(
+              //       'show date time picker (Chinese)',
+              //       style: TextStyle(color: Colors.blue),
+              //     ))
             ],
           ),
         ),
